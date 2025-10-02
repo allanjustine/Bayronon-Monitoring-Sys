7b04846ac4be623bb1a11168eace88c340a88c72
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-10-02 08:51:49 +0800

    dd

--- resources/views/livewire/pages/index.blade.php
+++ resources/views/livewire/pages/index.blade.php
@@ -13,7 +13,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5">
             @each('livewire.pages.list', $this->employees, 'employee', 'livewire.pages.empty')
         </div>
-        <footer class="bg-gray-700 p-5 w-full mt-5">
+        <footer class="bg-gray-800 p-5 w-full mt-5">
             <div class="w-full flex justify-center flex-col items-center text-sm">
                 <p>&copy; {{ date('Y') }} mga naay bayronon.</p>
                 <a href="{{ route('login') }}" wire:navigate class="text-blue-500 hover:text-blue-500 hover:underline">Login</a>
