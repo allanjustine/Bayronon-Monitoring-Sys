a647c5d6ae004cf674a868fe67ee643327880b45
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-10-02 16:05:20 +0800

    dd

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
     ],
     server: {
         cors: true,
-    }
+    },
+    build: {
+        outDir: "public/flux",
+        rollupOptions: {
+            input: "resources/js/flux.js",
+        },
+    },
 });
