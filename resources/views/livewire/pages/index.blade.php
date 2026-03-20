17cd23e7f6574602808f69e2ddb84d503ced6b10
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-03-20 10:05:26 +0800

    added filter by and also added skeleton

--- resources/views/livewire/pages/index.blade.php
+++ resources/views/livewire/pages/index.blade.php
@@ -10,7 +10,47 @@
         </div>
     </div>
     <div class="w-full overflow-y-auto h-[calc(100vh-90px)] pt-5 px-5">
+        <div class="flex justify-end mb-1">
+            <div class="flex gap-1">
+                <flux:button type="button" wire:click="handleFilter('is_utangan')"
+                    class="{{ $this->is_utangan ? 'bg-blue-600!' : 'bg-blue-500!' }}" variant="ghost">
+                    {{ $this->is_utangan ? 'Clear filter' : 'Filter by utangan' }}</flux:button>
+                <flux:button type="button" wire:click="handleFilter('is_kuwangan')"
+                    class="{{ $this->is_kuwangan ? 'bg-blue-600!' : 'bg-blue-500!' }}" variant="ghost">
+                    {{ $this->is_kuwangan ? 'Clear filter' : 'Filter by kuwangan' }}</flux:button>
+            </div>
+        </div>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5">
+            @foreach (range(1, 10) as $item)
+                <div class="w-full" wire:loading wire:target='handleFilter,search'>
+                    <flux:card class="bg-white dark:bg-gray-800" wire:key='{{ $item }}'>
+                        <div class="flex flex-col gap-6">
+                            <div class="flex gap-12">
+                                <div>
+                                    <flux:skeleton class="w-42 h-7"></flux:skeleton>
+
+                                    <flux:skeleton size="xl" class="mt-2 tabular-nums"></flux:skeleton>
+                                </div>
+
+                                <div>
+                                    <flux:skeleton class="w-22"></flux:skeleton>
+
+                                    <flux:skeleton size="lg" class="mt-2 tabular-nums"></flux:skeleton>
+                                </div>
+                            </div>
+                            <div class="flex justify-between h-18 gap-1">
+
+                                <flux:skeleton animate="shimmer" class="aspect-[4/1] size-full rounded-lg" />
+
+
+                                <flux:skeleton animate="shimmer" class="aspect-[4/1] size-full rounded-lg" />
+                            </div>
+
+                            <flux:skeleton animate="shimmer" class="aspect-[4/1] size-full h-12 rounded-lg" />
+                        </div>
+                    </flux:card>
+                </div>
+            @endforeach
             @forelse ($this->employees as $employee)
                 @include('livewire.pages.list', [$employee, $amount])
             @empty
